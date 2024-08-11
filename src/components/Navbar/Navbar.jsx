8a061d00afec19c885d33ca155ea9ec0bc0901cf
Sakshi Dappu <dappusakshi@gmail.com>
2024-08-12 00:18:51 +0530

    Bug Fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Portfolio
+      CraftedBySakshi
       </a>
       <div className={styles.menu}>
         <img
